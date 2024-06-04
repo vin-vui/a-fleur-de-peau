23f4de77c32745874084cacea913debe4bd1381b
hhhhhsssssh <160716860+hhhhhsssssh@users.noreply.github.com>
2024-06-04 15:46:27 +0200

    je veux pas perdre mon travail

---
+++ resources/js/Layouts/GuestLayout.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+import { Head, Link, router } from '@inertiajs/vue3';
+
+defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div>
+        <Head :title="title" />
+
+        <Banner />
+
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
